903f109d815ccc413a03c7320323181678badfbc
Arthur Endlein <endarthur@gmail.com>
2017-07-08 15:12:35 -0300

    added docstring for stress.michael
    Doctring created using numpydoc convention. Seems OK for the library, though may be interesting to check other conventions.

--- auttitude/stress.py
+++ auttitude/stress.py
@@ -6,7 +6,39 @@ from .models import LineData
 
 
 def michael(planes, lines):
-    # From Michael (1984)
+    """Linear paleostress inversion by Michael (1984) method.
+
+    Estimate paleostress from fault data using the method detailed in
+    Michael (1984). Needs at least two faults (plane/line pairs) to work.
+
+    Args:
+        planes: (M, 3) array_like
+                An array-like containing the direction cosines of the fault
+                planes.
+        lines: (M, 3) array_like
+               An array-like containing the direction consines of the fault
+               lines, respective to each plane.
+
+    Returns:
+        stress_matrix: (3, 3) ndarray
+                       Least-squares solution to the inversion method.
+        residual: float
+                  Sum of residuals for the solution.
+
+    Notes
+    -----
+    To linearize the inversion problem, _[1] assumes a priori that the
+    tangential traction magnitude is constant on each fault plane, and as only
+    relative stresses may be obtained by inversion, sets this traction to 1.
+    This allows for a least squares solution to the problem.
+
+
+    References
+    ----------
+    .. [1] Michael, A. J. (1984). Determination of stress from slip data:
+           faults and folds. Journal of Geophysical Research: Solid Earth,
+           89(B13), 11517-11526.
+    """
     # pylint: disable=line-too-long, invalid-name
     A = np.zeros((3*len(planes), 5))
     for i, (n1, n2, n3) in enumerate(planes):
@@ -18,7 +50,7 @@ def michael(planes, lines):
     stress_matrix = np.array(((s11, s12, s13),
                               (s12, s22, s23),
                               (s13, s23, -(s11 + s22))))
-    return stress_matrix, residuals.view(np.ndarray)
+    return stress_matrix, residuals[0]
 
 
 def principal_stresses(stress_matrix):
