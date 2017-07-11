8ec62a318a86997c5c49524c98e3221dc28b417b
Arthur Endlein <endarthur@gmail.com>
2017-07-11 14:18:32 -0300

    refactored grid to remove duplication
    Added a generic count method that receives and passes to a given function args and kwargs.

--- auttitude/grid.py
+++ auttitude/grid.py
@@ -46,17 +46,7 @@ class SphericalGrid(object):
         Will estimate an appropriate k if not given."""
         if k is None:
             k = optimize_k(data)  # Its a better estimate than R&J 86.
-        try:  # Try calculating directly with numpy arrays
-            return np.exp(k * (np.abs(np.dot(self.grid, np.transpose(data)))
-                               - 1)).sum(axis=1)
-        except (MemoryError, ValueError):
-            result = np.zeros(self.grid.shape[0])
-            for i, input_node in enumerate(self.grid):
-                result[i] = np.exp(k *
-                                        (np.abs(np.dot(input_node,
-                                                np.transpose(data)))
-                                         - 1)).sum()
-        return result
+        return self.count(data, fisher_distance, k)
 
     def count_kamb(self, data, theta=None):
         """Performs data counting as in Robin and Jowett (1986) based on
@@ -66,17 +56,25 @@ class SphericalGrid(object):
             theta = (len(data)-1.0)/(len(data)+1.0)
         else:
             theta = cos(radians(theta))
-        try:
-            return np.where(np.abs(np.dot(self.grid,
-                                          np.transpose(data)))
-                            >= theta, 1, 0).sum(axis=1)
+        return self.count(data, angle_treshold, theta)
+
+    def count(self, data, function, *args, **kwargs):
+        """Generic counting grid method that accepts a function which
+        receives the grid (or a node in the grid), the data and the additional
+        arguments and keyword arguments passed to this method."""
+        try:  # Try calculating directly with numpy arrays
+            return function(self.grid, data, *args, **kwargs).sum(axis=1)
         except (MemoryError, ValueError):
             result = np.zeros(self.grid.shape[0])
             for i, input_node in enumerate(self.grid):
-                result[i] = np.where(np.abs(np.dot(input_node,
-                                                        np.transpose(data)))
-                                          >= theta, 1, 0).sum()
+                result[i] = function(input_node, data, *args, **kwargs).sum()
         return result
+
+def fisher_distance(nodes, data, k):
+    return np.exp(k * (np.abs(np.dot(nodes, np.transpose(data))) - 1))
+
+def angle_treshold(nodes, data, theta):
+    return np.where(np.abs(np.dot(nodes, np.transpose(data))) >= theta, 1, 0)
 
 
 default_grid = SphericalGrid(node_spacing=2.5)
