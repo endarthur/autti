a3dce471e688b9168d3aeb3b9ecabe94b109afa8
Arthur Endlein <endarthur@gmail.com>
2017-07-11 18:10:53 -0300

    Implemented translation of attitudes at conversion
    Added attitude translation and an example to the notebook (fractures.txt).

--- auttitude/__init__.py
+++ auttitude/__init__.py
@@ -8,6 +8,7 @@ from geometry import dcos_plane, sphere_plane, dcos_line, dcos_rake,\
                      small_circle_intersection
 from grid import SphericalGrid, default_grid
 from plot import ProjectionPlot
+from conversion import translate_attitude
 
 import stress
 import sample
